ae680da1ce7ab7bddf683a8c3b2b33315eef4e0f
anyimed <115967791+anyimed@users.noreply.github.com>
2022-10-29 12:04:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -177,7 +177,7 @@ const websocket = {
       msg.message = filter.clean(msg.message); //Don't be an ******
       // checkword(msg.message)
       console.log(msg)
-      let obj = { user: msg.user, message: msg.message, image: msg.image };
+      let obj = { user: msg.user, message: msg.message, image: msg.image, unique: msg.unique  };
       const { data, error } = await supabase.from("message").insert([obj]);
       if (!error) {
         if (websocket.temp.length >= websocket.limit) {
