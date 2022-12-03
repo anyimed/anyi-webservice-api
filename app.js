12799e139b8a596f0ee02f8d919a06a786cd1786
anyimed <115967791+anyimed@users.noreply.github.com>
2022-12-03 14:57:27 +0700

    UPDATE SERVBICE

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var app = express();
+const bodyParser = require('body-parser');
+app.use(express.bodyParser());
+
 var expressWs = require("express-ws")(app);
 /********************************/
 const { createClient } = require("@supabase/supabase-js");
@@ -72,9 +75,9 @@ app.get("/clearcache", async function (req, res, next) {
 
 app.post("/url", async function (req, res, next) {
 console.log(req.body)
-//   websocket.url = req.body.url;
-//   return res.json({
-//     success: true, newUrl: websocket.url  });
+  websocket.url = req.body.url;
+  return res.json({
+    success: true, newUrl: websocket.url  });
 });
 app.get("/url", async function (req, res, next) {
   return res.json({ success: true, currentUrl: websocket.url });
@@ -91,9 +94,9 @@ app.get("/tell", async function (req, res, next) {
 
 app.post("/islive", async function (req, res, next) {
 console.log(req.body)
-//   websocket.islive = req.body.islive;
-//   return res.json({
-//     success: true, islive: websocket.isLive  });
+  websocket.islive = req.body.islive;
+  return res.json({
+    success: true, islive: websocket.isLive  });
 });
 app.get("/islive", async function (req, res, next) {
   return res.json({ success: true, islive: websocket.islive });
